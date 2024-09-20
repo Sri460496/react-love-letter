f6948533d7b40e4a16ca1e59883c7044d6f2176a
Sri460496 <sri460496@gmail.com>
2024-09-20 19:23:00 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+hiimport React, { useState, useRef } from 'react';
 import './LoveLetter.css';
 import audioFile from './kushi.mp3';
 
@@ -38,7 +38,7 @@ const LoveLetter = () => {
         mah dear kundanapu bomma🖤,<br />
         never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
         always yours,<br />
-        @niihaaarrrr
+        @srikanth
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
